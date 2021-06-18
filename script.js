72cba9f405e3e048f43b879870d42ea36df5dba2
saurav <kr.saurav.010@in.ibm.com>
2021-06-18 14:35:18 +0530

    modified as per specification

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
+    today_date = new Date();
+    
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    
+    amount = p * ( 100 + r*t)/100;
+    final_year = 1*t + today_date.getFullYear()
+    
+    out_html = "If you deposit " + p + "<br>" + "at an interst rate of " + r + "<br>" + "you will receive an amount of " + amount + "<br>" + "in the year " + final_year
+    
+    document.getElementById("result").innerHTML = out_html;
+    
     
 }
-        
+    
